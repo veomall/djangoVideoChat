fa5a048cb6908c734b90d2f82934391c2526ea02
Georgiy <gogapryst@gmail.com>
2024-10-15 00:46:48 +0300

    add more users

--- static/js/streams.js
+++ static/js/streams.js
@@ -9,6 +9,8 @@ let localTracks = []
 let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
+    client.on('user-published', handleUserJoined)
+
     UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
 
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
@@ -24,6 +26,32 @@ let joinAndDisplayLocalStream = async () => {
     localTracks[1].play(`user-${UID}`);
 
     await client.publish([localTracks[0], localTracks[1]])
+}
+
+
+let handleUserJoined = async (user, mediaType) => {
+    remoteUsers[user.uid] = user
+    await client.subscribe(user, mediaType)
+
+    if (mediaType === 'video') {
+        let player = document.getElementById(`user-container-${user.uid}`)
+        if (player != null) {
+            player.remove()
+        }
+
+        player = `<div class="video-container" id="user-container-${user.uid}">
+            <div class="username-wrapper">
+                <span class="user-name">My name</span>
+            </div>
+            <div class="video-player" id="user-${user.uid}"></div>
+        </div>`;
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+        user.videoTrack.play(`user-${user.uid}`);
+    }
+
+    if (mediaType === 'audio') {
+        user.audioTrack.play()
+    }
 }
 
 joinAndDisplayLocalStream()
